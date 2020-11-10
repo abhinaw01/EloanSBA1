5b12a76786a87b0a87930ccafb829de5c6fb9c14
abhinaw01 <kumarabhinaw773@gmail.com>
2020-11-10 17:55:06 +0530

    first commit

---
+++ src/main/java/com/iiht/evaluation/eloan/controller/AdminController.java
@@ -0,0 +1,103 @@
+package com.iiht.evaluation.eloan.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.iiht.evaluation.eloan.dao.ConnectionDao;
+import com.iiht.evaluation.eloan.dto.LoanDto;
+import com.iiht.evaluation.eloan.model.ApprovedLoan;
+import com.iiht.evaluation.eloan.model.LoanInfo;
+
+
+@WebServlet({"/loanapplicationlist","/sanctionedloandetails","/updatestatus","/logout"})
+public class AdminController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private ConnectionDao connDao;
+	
+	public void setConnDao(ConnectionDao connDao) {
+		this.connDao = connDao;
+	}
+	public void init(ServletConfig config) {
+		String jdbcURL = config.getServletContext().getInitParameter("jdbcUrl");
+		String jdbcUsername = config.getServletContext().getInitParameter("jdbcUsername");
+		String jdbcPassword = config.getServletContext().getInitParameter("jdbcPassword");
+		System.out.println(jdbcURL + jdbcUsername + jdbcPassword);
+		this.connDao = new ConnectionDao(jdbcURL, jdbcUsername, jdbcPassword);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action =  request.getParameter("action");
+		System.out.println(action);
+		String viewName = "";
+		try {
+			switch (action) {
+			case "loanapplicationlist" : 
+				viewName = loanapplicationlist(request, response);
+				break;
+			case "sanctionedloandetails":
+				viewName=sanctionedloandetails(request,response);
+				break;
+			case "updatestatus":
+				viewName=updatestatus(request,response);
+				break;
+			case "logout":
+				viewName = adminLogout(request, response);
+				break;	
+			default : viewName = "notfound.jsp"; break;		
+			}
+		} catch (Exception ex) {
+			throw new ServletException(ex.getMessage());
+		}
+		RequestDispatcher dispatch = 
+					request.getRequestDispatcher(viewName);
+		dispatch.forward(request, response);
+		
+		
+	}
+
+	private String updatestatus(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+		// TODO Auto-generated method stub
+		/* write the code for updatestatus of loan and return to admin home page */
+		
+		return null;
+	}
+	
+	private String sanctionedloandetails(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+		// TODO Auto-generated method stub
+	/* return to process page */
+		return  null;
+	}
+	private String adminLogout(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+	/* write code to return index page */
+		return null;
+	}
+
+	private String loanapplicationlist(HttpServletRequest request, HttpServletResponse response) throws SQLException {
+	/* write the code to display all the loans */
+		
+		return null;
+	}
+
+	
+}
